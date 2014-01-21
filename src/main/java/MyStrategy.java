16b87c9c5b807fef825f0a43e2b1d1fe0e622c60
darksotnea <shupov@mail.ru>
2014-01-21 15:17:03 +0400

    допилка

--- src/main/java/MyStrategy.java
+++ src/main/java/MyStrategy.java
@@ -1,5 +1,9 @@
 import model.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class MyStrategy implements Strategy {
     Trooper self;
     World world;
@@ -11,6 +15,11 @@ public class MyStrategy implements Strategy {
     CellType[][] cells;
     int H;
     int W;
+    Trooper commander;
+    Trooper medic;
+    Trooper soldier;
+    Trooper sniper;
+    Trooper scout;
 
     @Override
     public void move(Trooper self, World world, Game game, Move move) {
@@ -25,18 +34,61 @@ public class MyStrategy implements Strategy {
         H = world.getHeight();
         W = world.getWidth();
 
+        initMatch();
+        calculateMove();
+        finishMatch();
 
-
-
-        startMatch();
-        doMove();
     }
 
-    void startMatch() {
+    void initMatch() {
         //TODO задание начальной конфигурации, создание массивов, списков, коллекций и т. п.
+
     }
 
-    void doMove() {
+    void calculateMove() {
         //TODO перебор ходов и выбор наилучшего исходя из заданной статистики
+    }
+
+    void finishMatch() {
+        //TODO конечные вычисления
+    }
+
+    public ArrayList<Trooper> getTeammates() {
+        commander = null;
+        medic = null;
+        soldier = null;
+        sniper = null;
+        scout = null;
+
+        ArrayList<Trooper> r = new ArrayList<>();
+        for (Trooper trooper : world.getTroopers()) {
+            if (trooper.isTeammate()) {
+                r.add(trooper);
+                switch (trooper.getType()) {
+                    case COMMANDER:
+                        commander = trooper;
+                        break;
+                    case FIELD_MEDIC:
+                        medic = trooper;
+                        break;
+                    case SOLDIER:
+                        soldier = trooper;
+                        break;
+                    case SNIPER:
+                        sniper = trooper;
+                        break;
+                    case SCOUT:
+                        scout = trooper;
+                        break;
+                }
+            }
+        }
+        Collections.sort(r, new Comparator<Trooper>() {
+            @Override
+            public int compare(Trooper o1, Trooper o2) {
+                return Long.compare(o1.getId(), o2.getId());
+            }
+        });
+        return r;
     }
 }
